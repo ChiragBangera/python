1a928ec0fbd1d5f20758b072e4d12582fe4f68d3
ChiragBangera <chiragbangera468@gmail.com>
2024-09-19 00:32:39 +0530

    lists test

---
+++ python_for_db/lists.py
@@ -0,0 +1,13 @@
+example_list = [1,2,34,5,6,7]
+
+print(len(example_list))
+
+example_list.append('10000')
+
+print(example_list)
+
+example_list_two = example_list[:4]
+
+combined_list = example_list + example_list_two
+
+print(combined_list)
