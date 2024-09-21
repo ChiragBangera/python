1153d5817ef2a2e614986465685d2ed1c01aa1d8
ChiragBangera <chiragbangera468@gmail.com>
2024-09-21 10:39:47 +0530

    support files

--- python_for_db/lists.py
+++ python_for_db/lists.py
@@ -1,4 +1,4 @@
-example_list = [1,2,34,5,6,7]
+example_list = [1, 2, 34, 5, 6, 7]
 
 print(len(example_list))
 
@@ -11,3 +11,12 @@ example_list_two = example_list[:4]
 combined_list = example_list + example_list_two
 
 print(combined_list)
+
+our_first_dictionary = {'key': 'value', 'key_2': 'value_2', 2: 4, 4: 9, 'Chirag': 'Bangera'}
+
+del our_first_dictionary['key']
+print(our_first_dictionary.values())
+print(our_first_dictionary.get('Chirag', 'Hunter'))
+
+our_first_dictionary.setdefault('Amazon', 0)
+print(our_first_dictionary)
